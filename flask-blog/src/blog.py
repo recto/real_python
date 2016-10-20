0efb706f47b807054de6ed4812337556ebedebc8
Itsuo Okamoto <itsuo.okamoto@recto.com>
2016-10-19 18:46:11 -0700

    flask-blog update 2016.10.19

--- flask-blog/src/blog.py
+++ flask-blog/src/blog.py
@@ -4,6 +4,7 @@ Flask blog application
 import sqlite3
 from flask import Flask, render_template, request, session, flash, \
         redirect, url_for, g
+from functools import wraps
 
 # configuration
 DATABASE = 'blog.db'
@@ -15,6 +16,17 @@ app = Flask(__name__)
 
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
+
+def login_required(test):
+    """ return wraps that checks if user logged in """
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 def connect_db():
     """function used for connecting to the database"""
@@ -37,11 +49,34 @@ def login():
     return render_template('login.html', error=error)
 
 @app.route('/main')
+@login_required
 def main():
     """
     return main page
     """
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    return render_template('main.html', posts=posts)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    """
+    Add a post to the blog.
+    """
+    title = request.form['title']
+    past = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+    else:
+        g.db = connect_db()
+        cur = g.db.execute('insert into posts (title, post) values (?, ?)', 
+                [title, post])
+        g.db.commit()
+        g.db.close()
+        flash("New entry was successfully posted!")
+        return redirect(url_for('main'))
 
 @app.route('/logout')
 def logout():
